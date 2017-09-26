d8498bbd69a6eb0c7eb972c7782627df7c9c4c39
Aspet Amirkhanian <aspeta@yahoo.com>
2017-09-26 16:20:27 +0000

    revised production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 # Added for devise
-config.action_mailer.default_url_options = { :host => 'localhost' }
+config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
